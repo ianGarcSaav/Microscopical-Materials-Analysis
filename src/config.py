7c00d3336352fd9c2b075408031d115982b52e45
INWHIT3 <153135128+ianGarcSaav@users.noreply.github.com>
2025-05-28 11:31:43 -0700

    Update image folder path in config.py to point to new directory structure

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-img_folder = os.path.join(base_path, "images/grains/12 Mpx 4608x3456px a 500X")  # images folder is outside src
+img_folder = os.path.join(base_path, "images/grains/Imagenes Base")  # images folder is outside src
 clusters_folder = os.path.join(base_path, "results")
 
 # Ensure directories exist
